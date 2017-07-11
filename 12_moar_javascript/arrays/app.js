02a0809213ebc51dece9774913cc88c66a2dbec0
sharesef <sharesefrederick@gmail.com>
2017-07-11 19:18:16 -0400

    First arrays

---
+++ 12_moar_javascript/arrays/app.js
@@ -0,0 +1,20 @@
+var colors = ["purple", "yellow", "silver", "black"];
+
+
+/* Print the first color*/
+console.log(colors[0]);
+
+/* Print the last color*/
+console.log(colors[3]);
+
+/* Print the length of the array*/
+console.log(colors.length);
+
+
+/* Print the I like X color for each color in the array*/
+
+console.log("I like " + colors[0]);
+console.log("I like " + colors[1]);
+console.log("I like " + colors[2]);
+console.log("I like " + colors[3]);
+/*var fruits = ["apple", "Orange", "Grape"]; */
